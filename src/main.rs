d8bdf31ad8af6377d791b8b7b37d26b5556c780a
José Augusto <josemoura212@gmail.com>
2024-01-07 16:09:17 -0300

    Add math functions to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+mod matematica {
+
+    pub fn somar(a: i32, b: i32) -> i32 {
+        a + b
+    }
+
+    pub fn subtrair(a: i32, b: i32) -> i32 {
+        a - b
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("2 + 2 = {}\n", matematica::somar(2, 2));
+    println!("2 - 2 = {}\n", matematica::subtrair(2, 2));
 }
