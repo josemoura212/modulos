90228e6df5f59213120877c8beb75105bb8082af
José Augusto <josemoura212@gmail.com>
2024-01-07 16:26:22 -0300

    Refactor matematica module import

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,4 @@
-mod matematica {
-
-    pub fn somar(a: i32, b: i32) -> i32 {
-        a + b
-    }
-
-    pub fn subtrair(a: i32, b: i32) -> i32 {
-        a - b
-    }
-}
+mod matematica;
 
 fn main() {
     println!("2 + 2 = {}\n", matematica::somar(2, 2));
---
+++ src/matematica.rs
@@ -0,0 +1,8 @@
+
+pub fn somar(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+pub fn subtrair(a: i32, b: i32) -> i32 {
+    a - b
+}
