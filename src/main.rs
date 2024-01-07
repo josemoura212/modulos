76112c59f3cbb94f9914149edec15bb19a973ebd
José Augusto <josemoura212@gmail.com>
2024-01-07 16:33:47 -0300

    Refactor import statements in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 mod operacoes;
 
+use operacoes::matematica;
+
+// use operacoes::matematica::somar;
+// use operacoes::matematica::subtrair;
 fn main() {
-    println!("2 + 2 = {}\n", operacoes::matematica::somar(2, 2));
-    println!("2 - 2 = {}\n", operacoes::matematica::subtrair(2, 2));
+    println!("2 + 2 = {}\n", matematica::somar(2, 2));
+    println!("2 - 2 = {}\n", matematica::subtrair(2, 2));
 }
