427d533b1f7deb34cabb0816bdaf6c95873d1688
José Augusto <josemoura212@gmail.com>
2024-01-07 16:31:34 -0300

    Refactor code to use nested module structure for operations

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-mod matematica;
+mod operacoes;
 
 fn main() {
-    println!("2 + 2 = {}\n", matematica::somar(2, 2));
-    println!("2 - 2 = {}\n", matematica::subtrair(2, 2));
+    println!("2 + 2 = {}\n", operacoes::matematica::somar(2, 2));
+    println!("2 - 2 = {}\n", operacoes::matematica::subtrair(2, 2));
 }
